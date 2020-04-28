d52362f6fea09486b3202ce0a6c1eb09f7d26c1d
Diegobolzel <diegobolzel@gmail.com>
2020-04-28 10:35:29 -0600

    preparing webpack + postcss

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+const path = require("path");
+const postCSSPlugings = [
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("autoprefixer"),
+];
+module.exports = {
+  //Script to watch
+  entry: "./app/assets/scripts/App.js",
+  //The result script
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
+  },
+  mode: "development",
+  //always watching
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader?url=false",
+          { loader: "postcss-loader", options: { plugins: postCSSPlugings } },
+        ],
+      },
+    ],
+  },
+};
