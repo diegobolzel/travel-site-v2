9ce6141dde89ee6c1a65cebce23a783ef8d84d6d
Diegobolzel <diegobolzel@gmail.com>
2020-06-02 14:04:07 -0600

    Completed Desktop and mobile Header styles

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const postCSSPlugings = [
   require("postcss-mixins"),
   require("postcss-simple-vars"),
   require("postcss-nested"),
+  require("postcss-hexrgba"),
   require("autoprefixer"),
 ];
 module.exports = {
