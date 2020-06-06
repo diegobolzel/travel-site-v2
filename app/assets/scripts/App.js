1b2c83ba4652eafac7442787e9209dfb7b7c03a7
Diegobolzel <diegobolzel@gmail.com>
2020-06-06 05:52:42 -0600

    Completed the Reveal On Scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import "../styles/styles.css";
 //alert("Hello, this is just a test.!!!");
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnscroll";
 
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 let mobileMenu = new MobileMenu();
 
 if (module.hot) {
